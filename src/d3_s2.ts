f8ad9a87831108374c0344fdfdbb9877f87f6452
Gyges <shepherd2sovereign@gmail.com>
2025-12-21 09:19:46 -0500

    Complete and o(n)

---
+++ src/d3_s2.ts
@@ -0,0 +1,44 @@
+const response = await fetch("https://adventofcode.com/2025/day/3/input", {
+  headers: {
+    Cookie: `session=${process.env.AOC_SESSION}`,
+  },
+});
+const input = await response.text();
+const test =
+  "987654321111111\n811111111111119\n234234234234278\n818181911112111";
+const NUM_BATTERIES = 12;
+
+function findLargestJoltage(bank: string): number {
+  let joltage: string[] = [];
+  let start = 0;
+  for (let i = 1; i <= NUM_BATTERIES; i++) {
+    const [digit, newIndex] = findLargestDigit(
+      bank.slice(start, bank.length - (NUM_BATTERIES - i)),
+    );
+    joltage.push(digit);
+    start += newIndex + 1;
+  }
+  return Number(joltage.join(""));
+}
+
+function findLargestDigit(sub_bank: string): [string, number] {
+  let largestDigit = -1;
+  let largestIndex = 0;
+  for (let i = sub_bank.length - 1; i >= 0; i--) {
+    const digit = Number(sub_bank[i]);
+    if (digit >= largestDigit) {
+      largestDigit = digit;
+      largestIndex = i;
+    }
+  }
+  return [largestDigit.toString(), largestIndex];
+}
+
+const answer = input
+  .trim()
+  .split("\n")
+  .map((bank) => findLargestJoltage(bank))
+  .reduce((sum, val) => sum + val, 0);
+
+console.log(answer);
+//bun run src/d3_s2.ts  0.02s user 0.01s system 21% cpu 0.160 total
