481edc7a31ccb76427d7a7f9c8e4168e470a8a74
Gyges <shepherd2sovereign@gmail.com>
2025-12-22 07:45:07 -0500

    d6_s1 morning

---
+++ src/d6_s2.ts
@@ -0,0 +1,45 @@
+const response = await fetch("https://adventofcode.com/2025/day/6/input", {
+  headers: {
+    Cookie: `session=${process.env.AOC_SESSION}`,
+  },
+});
+const input = await response.text();
+const test = `123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   + `;
+
+const problemArray: string[][] = input
+  .trim()
+  .split("\n")
+  .map((line) => line.trim().split(/\s+/));
+
+const problemType: string[] = problemArray[problemArray.length - 1];
+
+const numbers: number[][] = problemArray
+  .slice(0, -1)
+  .map((line) => line.map(Number));
+
+function doCephalopodMath(
+  col: number,
+  numbers: number[][],
+  problems: string[],
+): number {
+  // Each row should have the same number of columns as problem types
+  if (numbers[col]!.length !== problems.length) {
+    throw new Error(
+      "Mismatch between columns and problem types for column ${col}.",
+    );
+  }
+
+  // code here
+
+  return sum;
+}
+
+let total = 0;
+for (let col = 0; col < numbers[0]!.length; col++) {
+  total += doCephalopodMath(col, numbers, problemType);
+}
+
+console.log(total);
